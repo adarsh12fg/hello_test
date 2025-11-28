59891a2ccc939f0a3c3889ad65afd1bf56a80f19
adarsh12fg <adarshrajsinn@gmail.com>
2025-11-28 23:36:20 +0530

    Add files via upload

---
+++ problem_82.c
@@ -0,0 +1,12 @@
+//  Print each character of a string on a new line.
+#include <stdio.h>
+int main(){
+    char text[]="hello \0";
+    int i=0;
+    while(text[i] != '\0')
+    { 
+    printf("%c\n ",text[i]);
+    i++;
+    }
+    return 0;
+}
